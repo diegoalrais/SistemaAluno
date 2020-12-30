0fe0c97f04d157ddffa1032cb3e351aead47b7cc
DIEGO MORAIS <diegoalrais@gmail.com>
2020-12-29 21:19:10 -0300

    Adicionar arquivos de projeto.

---
+++ src/SGA.ApplicationCore/Entity/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGA.ApplicationCore.Entity
+{
+    class Aluno
+    {
+        public Aluno()
+        {
+
+        }
+
+        public int AlunoId { get; set; }
+        
+        public string Nome { get; set; }
+
+        public int DataNascimento { get; set; }
+
+        public string Ano { get; set; }
+
+        public char Turma { get; set; }
+
+    }
+}
